c453eb2e00fbe870a65dbd2adc8b21b40eb214fd
shrutiverma97 <shruti@dream11.com>
2022-01-26 18:40:23 +0530

    added new url version

--- zio-http/src/main/scala/zhttp/http/URL3.scala
+++ zio-http/src/main/scala/zhttp/http/URL3.scala
@@ -6,41 +6,41 @@ import java.net.URI
 import scala.jdk.CollectionConverters._
 import scala.util.Try
 
-sealed trait URL2 { self =>
+sealed trait URL3 { self =>
 
   def host: Option[String] = self match {
-    case a @ URL2.FromString(_)   => a.toCons.get.host
-    case URL2.Cons(_, kind, _, _) =>
+    case a @ URL3.FromString(_)   => a.toCons.get.host
+    case URL3.Cons(_, kind, _, _) =>
       kind match {
-        case URL2.Location.Relative      => None
-        case abs: URL2.Location.Absolute => Option(abs.host)
+        case URL3.Location.Relative      => None
+        case abs: URL3.Location.Absolute => Option(abs.host)
       }
   }
   def port: Option[Int]    = self match {
-    case a @ URL2.FromString(_)   => a.toCons.get.port
-    case URL2.Cons(_, kind, _, _) =>
+    case a @ URL3.FromString(_)   => a.toCons.get.port
+    case URL3.Cons(_, kind, _, _) =>
       kind match {
-        case URL2.Location.Relative      => None
-        case abs: URL2.Location.Absolute => Option(abs.port)
+        case URL3.Location.Relative      => None
+        case abs: URL3.Location.Absolute => Option(abs.port)
       }
   }
 
-  private[zhttp] def relative: URL2 = self match {
-    case a @ URL2.FromString(_)       => a.toCons.get.relative
-    case b @ URL2.Cons(_, kind, _, _) =>
+  private[zhttp] def relative: URL3 = self match {
+    case a @ URL3.FromString(_)       => a.toCons.get.relative
+    case b @ URL3.Cons(_, kind, _, _) =>
       kind match {
-        case URL2.Location.Relative => b
-        case _                      => b.copy(kind = URL2.Location.Relative)
+        case URL3.Location.Relative => b
+        case _                      => b.copy(kind = URL3.Location.Relative)
       }
   }
-  def toCons: Option[URL2] = self match {
-    case a @ URL2.FromString(_) => a.fromString.toOption
-    case b @ URL2.Cons(_, _, _, _) => Some(b)
+  def toCons: Option[URL3] = self match {
+    case a @ URL3.FromString(_) => a.fromString.toOption
+    case b @ URL3.Cons(_, _, _, _) => Some(b)
   }
 
   def asString: String = self match {
-    case URL2.FromString(string)   => string
-    case u @ URL2.Cons(_, _, _, _) => {
+    case URL3.FromString(string)   => string
+    case u @ URL3.Cons(_, _, _, _) => {
       def path: String = {
         val encoder = new QueryStringEncoder(s"${u.path.asString}${u.fragment.fold("")(f => "#" + f.raw)}")
         u.queryParams.foreach { case (key, values) =>
@@ -50,31 +50,30 @@ sealed trait URL2 { self =>
       }
 
       u.kind match {
-        case URL2.Location.Relative                     => path
-        case URL2.Location.Absolute(scheme, host, port) =>
+        case URL3.Location.Relative                     => path
+        case URL3.Location.Absolute(scheme, host, port) =>
           if (port == 80 || port == 443) s"${scheme.asString}://$host$path"
           else s"${scheme.asString}://$host:$port$path"
       }
     }
   }
-
 }
-object URL2 {
-  final case class FromString(string: String) extends URL2 { self =>
-    def fromString: Either[HttpError.BadRequest, URL2] = {
+object URL3 {
+  final case class FromString(string: String) extends URL3 { self =>
+    def fromString: Either[HttpError.BadRequest, URL3] = {
       Try(unsafeFromString).toEither match {
         case Left(_)      => Left(HttpError.BadRequest(s"Invalid URL: $string"))
         case Right(value) => Right(value)
       }
     }
-    def unsafeFromString: URL2                        = {
-      try {
-        val url = new URI(string)
-        if (url.isAbsolute) fromAbsoluteURI(url).orNull else fromRelativeURI(url).orNull
-      } catch { case _: Throwable => null }
-    }
+        def unsafeFromString: URL3                         = {
+          try {
+            val url = new URI(string)
+            if (url.isAbsolute) unsafeFromAbsoluteURI2(url) else unsafeFromRelativeURI2(url)
+          } catch { case _: Throwable => null }
+        }
 
-    private def unsafeFromAbsoluteURI2(uri: URI): URL2 = {
+    private def unsafeFromAbsoluteURI2(uri: URI): URL3 = {
 
       def portFromScheme(scheme: Scheme): Int = scheme match {
         case Scheme.HTTP  => 80
@@ -88,31 +87,25 @@ object URL2 {
       val host    = uri.getHost
 
       if (port != -1 && scheme != null && host != null)
-        URL2.Cons(
+        URL3.Cons(
           Path(uri.getRawPath),
-          URL2.Location.Absolute(scheme, host, port),
+          URL3.Location.Absolute(scheme, host, port),
           queryParams(uri.getRawQuery),
-          URL2.Fragment.fromURI(uri),
+          URL3.Fragment.fromURI(uri),
         )
       else null
     }
 
-    private def unsafeFromRelativeURI2(uri: URI): URL2 =
-      URL2.Cons(Path(uri.getRawPath), URL2.Location.Relative, queryParams(uri.getRawQuery), URL2.Fragment.fromURI(uri))
-
-    private def fromAbsoluteURI(uri: URI): Option[URL2] = {
-      Try(unsafeFromAbsoluteURI2(uri)).toOption
-    }
-
-    private def fromRelativeURI(uri: URI): Option[URL2] = Try(unsafeFromRelativeURI2(uri)).toOption
+    private def unsafeFromRelativeURI2(uri: URI): URL3 =
+      URL3.Cons(Path(uri.getRawPath), URL3.Location.Relative, queryParams(uri.getRawQuery), URL3.Fragment.fromURI(uri))
 
   }
   final case class Cons(
-    path: Path,
-    kind: URL2.Location = URL2.Location.Relative,
-    queryParams: Map[String, List[String]] = Map.empty,
-    fragment: Option[Fragment] = None,
-  ) extends URL2
+                         path: Path,
+                         kind: URL3.Location = URL3.Location.Relative,
+                         queryParams: Map[String, List[String]] = Map.empty,
+                         fragment: Option[Fragment] = None,
+                       ) extends URL3
 
   sealed trait Location
   object Location {
@@ -130,8 +123,8 @@ object URL2 {
     }
   }
 
-  def fromString(string: String): Either[HttpError.BadRequest, URL2] = URL2.FromString(string).fromString
-  def unsafeFromString(string: String): URL2                         = URL2.FromString(string)
+  def fromString(string: String): Either[HttpError.BadRequest, URL3] = URL3.FromString(string).fromString
+  def unsafeFromString(string: String): URL3                         = URL3.FromString(string)
 
   case class Fragment private (raw: String, decoded: String)
   object Fragment {
@@ -140,6 +133,6 @@ object URL2 {
       decoded <- Option(uri.getFragment)
     } yield Fragment(raw, decoded)
   }
-  def root: URL2 = URL2.Cons(!!)
+  def root: URL3 = URL3.Cons(!!)
 
 }
