e79811e6b31aaf70d29e9d183ab731200e850f8d
Tushar Mathur <tusharmath@gmail.com>
2022-01-26 18:20:38 +0530

    feature: add `Socket.empty`

--- zio-http/src/main/scala/zhttp/socket/Socket.scala
+++ zio-http/src/main/scala/zhttp/socket/Socket.scala
@@ -21,6 +21,7 @@ sealed trait Socket[-R, +E, -A, +B] { self =>
     case FMerge(sa, sb)              => sa(a) merge sb(a)
     case Succeed(a)                  => ZStream.succeed(a)
     case Provide(s, r)               => s(a).asInstanceOf[ZStream[R, E, B]].provide(r.asInstanceOf[R])
+    case Empty                       => ZStream.empty
   }
 
   def contramap[Z](za: Z => A): Socket[R, E, Z, B] = Socket.FCMap(self, za)
@@ -58,6 +59,16 @@ sealed trait Socket[-R, +E, -A, +B] { self =>
 
 object Socket {
   def collect[A]: PartialCollect[A] = new PartialCollect[A](())
+
+  /**
+   * Simply echos the incoming message back
+   */
+  def echo[A]: Socket[Any, Nothing, A, A] = Socket.collect[A] { case a => ZStream.succeed(a) }
+
+  /**
+   * Creates a socket that doesn't do anything.
+   */
+  def empty: Socket[Any, Nothing, Any, Nothing] = Socket.Empty
 
   def end: ZStream[Any, Nothing, Nothing] = ZStream.halt(Cause.empty)
 
@@ -102,4 +113,6 @@ object Socket {
   private final case class Provide[R, E, A, B](s: Socket[R, E, A, B], r: R) extends Socket[Any, E, A, B]
 
   private case object End extends Socket[Any, Nothing, Any, Nothing]
+
+  private case object Empty extends Socket[Any, Nothing, Any, Nothing]
 }
